afc89386710ccdee5b644a50c22a001fdbd423a5
Nikechukwu <nikechukwu@gmail.com>
2024-07-07 12:11:40 -0400

    fixed server code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
+import { h } from "https://deno.land/x/nano_jsx@v0.1.0/core.ts";
+
 export default {
-    handler: (req) => {
-        return (
-            <div>Hello</div>
-        )
-    }
-}
+  handler: (req) => {
+    return <div>Homepage</div>;
+  },
+};
--- server/index.tsx
+++ server/index.tsx
@@ -1,11 +1,11 @@
-/** @jsx h */
 import { extname } from "jsr:@std/path/extname";
 import { exists } from "jsr:@std/fs/exists";
 import { walk } from "jsr:@std/fs/walk";
 import { InternalError } from "../internal/lib/errors.ts";
 import { join } from "jsr:@std/path/join";
+import { toFileUrl } from "jsr:@std/path/to-file-url";
 
-import { buildRouter, getRouterParams } from "../internal/lib/router.ts"
+import { buildRouter, getRouterParams } from "../internal/lib/router.ts";
 import { renderSSR } from "https://deno.land/x/nano_jsx@v0.1.0/ssr.ts";
 import { render } from "https://deno.land/x/nano_jsx@v0.1.0/core.ts";
 import { h } from "https://deno.land/x/nano_jsx@v0.1.0/core.ts";
@@ -16,57 +16,63 @@ const publicPath = "./public";
 const routerMap = buildRouter(cwd);
 
 Deno.serve({
-    handler: async (req) => {
-        const url = new URL(req.url);
-        const pathname = url.pathname;
-        // const body = await req.json();
-        // const text = await req.text();
-        // const method = req.method;
-        
-        const match = Array.from(routerMap.entries()).find((v) => {
-            console.log(v[0], pathname)
-            return v[0].test(pathname);
+  handler: async (req) => {
+    const url = new URL(req.url);
+    const pathname = url.pathname;
+    // const body = await req.json();
+    // const text = await req.text();
+    // const method = req.method;
+
+    const match = Array.from(routerMap.entries()).find((v) => {
+      console.log(v[0], pathname);
+      return v[0].test(pathname);
+    });
+
+    if (match) {
+      const routeInfo = match[1];
+      const reqObj = {
+        path: pathname,
+        hash: url.hash,
+        params: getRouterParams(routeInfo.original, routeInfo.raw, pathname),
+        query: convertSearchParams(url.searchParams),
+        fullPath: pathname + url.search + url.hash,
+        meta: {},
+      };
+
+      console.log(reqObj);
+
+      const response = import(toFileUrl(join(cwd, "pages", routeInfo.raw)).href)
+        .then((pagefile) => {
+          console.log(pagefile);
+          const Page = pagefile.default.handler;
+
+          const pageOut = renderSSR(() => <Page {...reqObj} />);
+          console.log(pageOut);
+          return pageOut;
         });
 
-        if (match) {
-            const routeInfo = match[1];
-            const reqObj = {
-                path: pathname,
-                hash: url.hash,
-                params: getRouterParams(routeInfo.original, routeInfo.raw, pathname),
-                query: convertSearchParams(url.searchParams),
-                fullPath: pathname + url.search + url.hash,
-                meta: {}
-            };
-
-            console.log(reqObj);
-
-            const pagefile = await import(join(cwd, 'pages', routeInfo.raw));
-            
-            const Page = pagefile.handler(reqObj);
-
-            const pageOut = renderSSR(<Page />);
-            return new Response(pageOut);
-        }
-        
-        
-
-        
-        return new Response("Hello World");
-    },
-    onListen({ port, hostname }) {
-        console.log(`Server started at http://${hostname}:${port}`);
-    },
-    onError(err) {
-        const sysError = err instanceof InternalError;
-        const error = sysError ? err as InternalError : new InternalError({});
-
-        return new Response(error.message, {
-            status: error.statusCode,
-            statusText: error.name
-        });
+      return new Response(await response, {
+        headers: {
+          "content-type": "text/html",
+        },
+      });
     }
-})
+
+    return new Response("Hello World");
+  },
+  onListen({ port, hostname }) {
+    console.log(`Server started at http://${hostname}:${port}`);
+  },
+  onError(err) {
+    const sysError = err instanceof InternalError;
+    const error = sysError ? err as InternalError : new InternalError({});
+
+    return new Response(error.message, {
+      status: error.statusCode,
+      statusText: error.name,
+    });
+  },
+});
 
 function convertSearchParams(searchParams: URLSearchParams) {
   const obj = {};
@@ -77,9 +83,13 @@ function convertSearchParams(searchParams: URLSearchParams) {
     if (value === "true") value = true;
     if (value === "false") value = false;
     else {
-        try { value = parseInt(value); } catch (_) {
-            try { value = parseFloat(value); } catch (_) { /* Do nothing */}
-        }
+      try {
+        value = parseInt(value);
+      } catch (_) {
+        try {
+          value = parseFloat(value);
+        } catch (_) { /* Do nothing */ }
+      }
     }
     Object.defineProperty(obj, key, { value, writable: false });
   }
