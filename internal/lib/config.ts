e6209301ea2327f97c8ae35108b8c02e07ba68c5
Nikechukwu <nikechukwu@gmail.com>
2024-07-07 21:24:46 -0400

    added config

---
+++ internal/lib/config.ts
@@ -0,0 +1,43 @@
+import { AppEntryOptions } from "../types/templates.ts"
+
+export interface NikeConfig {
+    app?: AppEntryOptions;
+    server?: {
+        host?: string;
+        port?: number;
+    },
+    publicDir?: string;
+    build?: {
+        singleBundle?: boolean;
+    };
+}
+
+export function defaultConfig(): NikeConfig {
+    return {
+        app: {
+            title: "My App",
+            head: {
+                meta: [
+                    {
+                        charset: "UTF-8"
+                    },
+                    {
+                        name: "viewport",
+                        content: "width=device-width, initial-scale=1.0"
+                    }
+                ]
+            }
+        },
+        server: {
+            port: 3000,
+            host: "localhost"
+        }
+    }
+}
+
+export function mergeConfig(configA: NikeConfig, configB: NikeConfig, precedence: 'a' | 'b' = 'a') {
+    const aPred = precedence === 'a';
+    return aPred ? {...configB, ...configA} : {...configA, ...configB};
+}
+
+export function defineConfig(options: NikeConfig) { return options; }
---
+++ nike.config.ts
@@ -0,0 +1,11 @@
+import { NikeConfig } from "#module";
+
+/** @type {NikeConfig} */
+export default {
+    app: {
+        title: "Nike Okoronkwo",
+    },
+    build: {
+        singleBundle: false 
+    }
+}
