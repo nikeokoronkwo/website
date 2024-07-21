def58868f3dfcb42228f1f3105948820358068e6
--add <nikechukwu@gmail.com>
2024-07-21 15:08:34 -0400

    svg optimization

--- components/iconList.tsx
+++ components/iconList.tsx
@@ -1,17 +1,12 @@
 import { FC } from "#client";
-
-export interface RedirectObject {
-  name?: string;
-  id: string;
-  url: string;
-}
+import { RedirectObject } from "~/lib/links.ts";
 
 const Icons: FC<{ list: RedirectObject[]; iconSize?: string }> = (
   { list, iconSize },
 ) => (
   <>
     {list.map((l) => (
-      <a href={l.url} target="_blank">
+      <a href={l.email ? `mailto:${l.url}` : l.url} target="_blank">
         <img
           src={`/assets/svg/${l.id === "pub" ? "dart" : l.id}.svg`}
           className={"aspect-square " + (iconSize ?? "h-10")}
