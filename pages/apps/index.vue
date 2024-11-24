52376e8b0f1b54bba83d730d13a10dca700cebc2
nikechukwu <nikechukwu@googlemail.com>
2024-11-24 02:35:44 +0000

    added applications showcase

---
+++ pages/apps/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+useHead({
+  title: "Applications",
+});
+</script>
+
+<template>
+  <div>
+    <div
+      class="items-center justify-center text-center py-8 text-3xl font-semibold"
+    >
+      Applications
+    </div>
+    <div class="flex flex-col w-screen px-5 space-y-5 items-center">
+      <AppSummary
+        v-for="app in apps"
+        :key="app.name"
+        :app="app"
+      />
+    </div>
+  </div>
+</template>
--- utils/index.ts
+++ utils/index.ts
@@ -1,29 +1,36 @@
 import p from "~/assets/json/projects.json" with { type: "json" };
+import a from "~/assets/json/apps.json" with { type: "json" };
 
-interface RedirectObject {
+export interface RedirectObject {
   name?: string;
   id: string;
   url: string;
   email?: boolean;
 }
 
-const links: RedirectObject[] = [
+export const links: RedirectObject[] = [
   { name: "Github", id: "github", url: "https://github.com/nikeokoronkwo" },
   { name: "Patreon", id: "patreon", url: "https://patreon.com/nikechukwu" },
   { name: "Email", id: "email", url: "nikechukwu@gmail.com", email: true },
 ];
 
-export interface Project {
+interface BaseProject {
   name: string;
   description?: string;
   img?: string;
   route?: string;
   inProgress?: boolean;
   directs?: RedirectObject[];
-  languages?: string[];
 }
 
-const projects: Project[] = p;
+export interface Project extends BaseProject {
+  languages: string[];
+}
 
-export { links, projects };
-export type { RedirectObject };
+export const projects: Project[] = p;
+
+export interface Application extends BaseProject {
+  platforms: string[];
+}
+
+export const apps: Application[] = a;
